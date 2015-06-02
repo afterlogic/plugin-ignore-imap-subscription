f616fe5cce87ccec1919dbde145ac96bfa313bca
support@afterlogic.com <support@afterlogic.com>
2015-06-02 17:18:04 +0300

    add files

---
+++ ignore-imap-subscription/index.php
@@ -0,0 +1,34 @@
+<?php
+
+/* -AFTERLOGIC LICENSE HEADER- */
+
+class_exists('CApi') or die();
+
+class CIgnoreImapSubscriptionPlugin extends AApiPlugin
+{
+	/**
+	 * @param CApiPluginManager $oPluginManager
+	 */
+	public function __construct(CApiPluginManager $oPluginManager)
+	{
+		parent::__construct('1.0', $oPluginManager);
+
+		$this->AddHook('api-change-account-by-id', 'PluginChangeAccountById');
+	}
+	
+	/**
+	 * @param CAccount $oAccount
+	 */
+	public function PluginChangeAccountById(&$oAccount)
+	{
+		$sEmail = CApi::GetConf('plugins.ignore-imap-subscription.options.email', '');
+		if ($oAccount && $oAccount instanceof CAccount &&
+			(empty($sEmail) || $sEmail === $oAccount->Email))
+		{
+			$oAccount->enableExtension(CAccount::IgnoreSubscribeStatus);
+			$oAccount->enableExtension(CAccount::DisableManageSubscribe);
+		}
+	}
+}
+
+return new CIgnoreImapSubscriptionPlugin($this);
